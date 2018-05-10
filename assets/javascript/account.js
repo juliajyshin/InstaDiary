a17d0bdf709dba17dc0fa5879fe6f53214414850
Julia Shin <juliashin@Julias-iMac.attlocal.net>
2018-05-10 15:40:06 -0700

    fire base upload image

--- assets/javascript/account.js
+++ assets/javascript/account.js
@@ -1,62 +1,61 @@
-var imgFile
+// var imgFile
 var userName
 var syncUserObject
 // Variables 
 var selectedFile
 
 // Changing a profile picture
-// $('#gettingImgFile').on('change', previewFile)
+$('#profile-upload').on('change', previewFile)
 
-// function previewFile() {
-//   var preview = document.querySelector('img')
-//   imgFile = document.querySelector('input[type=file]').files[0]
-//   console.log(imgFile.name)
-//   reader = new FileReader()
+function previewFile() {
+  var preview = document.querySelector('img')
+  selectedFile = document.querySelector('input[type=file]').files[0]
+  console.log(selectedFile.name)
+  reader = new FileReader()
+// Upload the photo and show a preview image
+  reader.addEventListener('load', function () {
+    $('#prof-upload').attr('src', reader.result)
+  }, false)
 
-
-//   reader.addEventListener('load', function () {
-//     $('#preview').attr('src', reader.result)
-//   }, false)
-
-//   if (imgFile) {
-//     reader.readAsDataURL(imgFile)
-
-//   }
-// }
+  if (selectedFile) {
+    reader.readAsDataURL(selectedFile)
+  }
+}
 
 
-// $('#saveButton').on('click', function () {
-//   uploadPhotoToDatabase()
-// })
-
-
-$("#profile-upload").on("change", function (event) {
-  selectedFile = event.target.files[0];
-  $("#saveButton").show();
+$('#saveButton').on('click', function () {
+  // selectedFile = event.target.files[0];
+  uploadProfileImage()
 })
+
+
+// $("#profile-upload").on("change", function (event) {
+//   selectedFile = event.target.files[0];
+//   $("#saveButton").show();
+// })
 
 // Upload Profile Image to Firebase
 function uploadProfileImage() {
-    // Create a root reference
-    var filename = selectedFile.name;
-    var storageRef = firebase.storage().ref('/profileImages/' + filename);
-    var uploadTask = storageRef.put(selectedFile);
+  // Create a root reference
+  var filename = selectedFile.name;
+  var storageRef = firebase.storage().ref('/profileImages/' + filename);
+  var uploadTask = storageRef.put(selectedFile);
 
-    // Register three observers:
-    // 1. 'state_changed' observer, called any time the state changes
-    // 2. Error observer, called on failure
-    // 3. Completion observer, called on successful completion
-    uploadTask.on('state_changed', function (snapshot) {
-      // Observe state change events such as progress, pause, and resume
-    }, function (error) {
-      // Handle unsuccessful uploads
-    }, function () {
-      // Handle successful uploads on complete
-      uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-        console.log('File available at', downloadURL);
-      });
+  // Register three observers:
+  // 1. 'state_changed' observer, called any time the state changes
+  // 2. Error observer, called on failure
+  // 3. Completion observer, called on successful completion
+  uploadTask.on('state_changed', function (snapshot) {
+    // Observe state change events such as progress, pause, and resume
+  }, function (error) {
+    // Handle unsuccessful uploads
+  }, function () {
+    // Handle successful uploads on complete
+    uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+      console.log('File available at', downloadURL);
     });
-  }
+  });
+}
 
 // function uploadPhotoToDatabase() {
 //     firebase.auth().onAuthStateChanged(function (user) {
@@ -102,55 +101,55 @@ function uploadProfileImage() {
 
 $(document).ready(function () {
 
-    $('#logoutButton').on('click', function () {
-      firebase.auth().signOut().then(function () {
-        document.location.href = 'Login.html'
-      }).catch(function (error) {
+  $('#logoutButton').on('click', function () {
+    firebase.auth().signOut().then(function () {
+      document.location.href = 'Login.html'
+    }).catch(function (error) {
+
+    })
+  })
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      database.ref('/user/' + user.uid).once('value', function (snap) {
+        var userObject = snap.val()
+        var userName = userObject.name
+        var userEmail = userObject.email
+        var profilePic
+
+
+        $('#userName').text(userName)
+        $('#userEmail').text(userEmail)
+
+        $('#titleUserName').text(userName)
+        $('#userNameNavBar').text(userName)
+        $('#userEmailNavBar').text(userEmail)
+
+
+
+
+        if (userObject.profilePicture == undefined) {
+          $('#preview').attr('src', "assets/images/default_profile.png")
+        } else {
+          profilePic = userObject.profilePicture.profile
+          $('#preview').attr('src', profilePic)
+        }
+
+        /*        if (userObject.profilePicture.profile != undefined || userObject.profilePicture.profile != null){
+                  profilePic = userObject.profilePicture.profile
+                  $('#preview').attr('src', profilePic)
+                } else {
+                  $('#preview').attr('src', "assets/images/default_profile.png")
+                }*/
+
+
 
       })
-    })
 
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        database.ref('/user/' + user.uid).once('value', function (snap) {
-          var userObject = snap.val()
-          var userName = userObject.name
-          var userEmail = userObject.email
-          var profilePic
-
-
-          $('#userName').text(userName)
-          $('#userEmail').text(userEmail)
-
-          $('#titleUserName').text(userName)
-          $('#userNameNavBar').text(userName)
-          $('#userEmailNavBar').text(userEmail)
-
-
-
-
-          if (userObject.profilePicture == undefined) {
-            $('#preview').attr('src', "assets/images/default_profile.png")
-          } else {
-            profilePic = userObject.profilePicture.profile
-            $('#preview').attr('src', profilePic)
-          }
-
-          /*        if (userObject.profilePicture.profile != undefined || userObject.profilePicture.profile != null){
-                    profilePic = userObject.profilePicture.profile
-                    $('#preview').attr('src', profilePic)
-                  } else {
-                    $('#preview').attr('src', "assets/images/default_profile.png")
-                  }*/
-
-
-
-        })
-
-      } else {
-        document.location.href = 'Login.html'
-      }
-
-    })
+    } else {
+      document.location.href = 'index.html'
+    }
 
   })
+
+})
