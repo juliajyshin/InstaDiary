be16ac26ea3109955275e215347c675421274657
Julia Shin <juliajyshin@gmail.com>
2019-03-06 19:33:14 -0800

    new map api key

--- public/assets/javascript/firebase.js
+++ public/assets/javascript/firebase.js
@@ -1,7 +1,6 @@
 
 var config = {
-    // apiKey: "AIzaSyAW5ecCR-5hW5d7_7swntc17s5w4OhX9UE",
-     apiKey: "AIzaSyD_DmtMWOESTCukIdrmomST6ptSAWy-CaA",
+    apiKey: "AIzaSyAW5ecCR-5hW5d7_7swntc17s5w4OhX9UE",
     authDomain: "instadiary-3c794.firebaseapp.com",
     databaseURL: "https://instadiary-3c794.firebaseio.com",
     projectId: "instadiary-3c794",
